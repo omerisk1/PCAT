739a066f5caa4b608c5f7da36d0a88da284cbae5
Ömer IŞIK <isikomer373@gmail.com>
2022-06-15 16:45:02 +0300

    template engine created.

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const ejs = require("ejs");
 const app = express();
 const port = 3000;
 const path = require("path");
 
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//MIDDLEWARES
 app.use(express.static("public"));
 
+//ROUTE
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render('index');
 });
+app.get("/about", (req, res) => {
+    res.render('about');
+  });
+  app.get("/add", (req, res) => {
+    res.render('add');
+  });
 
 app.listen(port, () => {
   console.log(`${port} una baglanildi.`);
